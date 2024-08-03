32b1dcf84942447b2cbc6d2d3dc1320ed909d1cc
danimora2027 <reparacionphonecel@gmail.com>
2024-08-02 20:49:04 -0500

    Comienzo de conexion de firebase con webhook

---
+++ functions/index.js
@@ -0,0 +1,11 @@
+const functions = require("firebase-functions");
+const express = require("express");
+
+const app = express();
+
+app.get("/hello-world", (req, res) => {
+    return res.status(200).json({ message: "Hello World!" });
+});
+
+app.use(require('./routes/portales.routes'));
+exports.app = functions.https.onRequest(app);
---
+++ functions/routes/portales.routes.js
@@ -0,0 +1,87 @@
+const { Router } = require('express');
+const admin = require("firebase-admin");
+const router = Router()
+
+//importante este metodo para la seguridad de la base de datos en firebase
+admin.initializeApp({
+    credential: admin.credential.cert("./credentials.json"),
+    databaseURL: "https://chatbotportalesinteracti-52820-default-rtdb.firebaseio.com"
+})
+
+const db = admin.firestore();
+// insertar registros 
+router.post("/api/frequencyquestions", async(req, res) => {
+    try {
+        await db.collection("frequentQuestions")
+            .doc('/' + req.body.id + '/')
+            .create({
+                answer: req.body.answer,
+                ask: req.body.ask,
+                idPortal: req.body.idPortal,
+                url: req.body.url
+            });
+
+        return res.status(204).json();
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+})
+
+//consultar registros de una colección 
+router.post("/api/certificado/:iddoccert", async(req, res) => { //Este es el codigo que necesitamos, ahora toca sacar el dato necesario
+    try {
+        const doc = db.collection('Certificado').doc(req.params.iddoccert)
+        const certificados = await doc.get();
+        const docCertificado = "Este es el certificado de: " + certificados.data().idEstudiante +
+            ". Se genera el siguiente certificado: " + certificados.data().docCertificado;
+        return res.status(200).json(docCertificado);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+});
+
+//multi consulta de registros 
+router.post("/api/multifreqquest", async(req, res) => {
+    try {
+        const query = db.collection('frequentQuestions');
+        const querySnapshot = await query.get();
+        const docs = querySnapshot.docs;
+
+        const response = docs.map((doc) => ({
+            id: doc.id,
+            answer: doc.data().answer,
+        }));
+
+
+        return res.status(200).json(response);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+});
+
+
+//eliminar registros 
+router.delete("/api/eliminar/:idfreqquest", async(req, res) => {
+    try {
+        const document = db.collection("frequentQuestions").doc(req.params.idfreqquest);
+        await document.delete();
+        return res.status(200).json();
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+});
+
+//actualizar registros 
+router.put("/api/actualizar/:idfreqquest", async(req, res) => {
+    try {
+        const document = db.collection("frequentQuestions").doc(req.params.idfreqquest);
+        await document.update({
+            idPortal: req.body.idPortal,
+        });
+        return res.status(200).json();
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+});
+
+module.exports = router
